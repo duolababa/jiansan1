94490aa03bd8d346ccdd71d41748adc958381335
pan97550036 <pan97550036@qq.com>
2023-07-21 20:20:27 +0800

    4

--- dll/luaFunc.cpp
+++ dll/luaFunc.cpp
@@ -2727,7 +2727,7 @@ static int 测试(__LUA_指针)
 
 	for (size_t i = 0; i < vsk.size(); i++)
 	{
-		MyTrace(L"位置%d  物品ID 0x%I64X 资源ID  0x%I64X 数量%d  箱子类型%d 耐久%0.3f  名称 %s \r\n", vsk2[i].dindex, vsk2[i].ItemId,vsk2[i].ItemResId, vsk2[i].dNum, vsk2[i].dSlotIndex, vsk2[i].耐久度.当前耐久度,vsk2[i].ItemName);
+		MyTrace(L"位置%d  物品ID 0x%I64X 资源ID  0x%I64X 数量%d  箱子类型%d 耐久%d  名称 %s \r\n", vsk2[i].dindex, vsk2[i].ItemId,vsk2[i].ItemResId, vsk2[i].dNum, vsk2[i].dSlotIndex, vsk2[i].耐久度.当前耐久度,vsk2[i].ItemName);
 		/*wchar_t buf[MAX_PATH];
 		swprintf(buf, L"位置%X 地址0x%I64X 物品ID 0x%I64X 资源ID %X 地址0x%I64X 数量%d %s 槽位%X 穿戴参数%d 箱子类型%d 耐久%d/%d\r\n", i, dStart + i * dSize, dItemId, dItmeResId, dItemResAddr, dNum, csName, dSlotIndex, getEquipWearArg(dSlotIndex), dBoxType, temp.耐久度.当前耐久度, temp.耐久度.最大耐久度);
 		MyTrace(buf);*/
